507bf963b5a5390baedd53f98c213d788861eb54
Daniel Roe <daniel@concision.co.uk>
2018-10-16 14:28:16 +0100

    Update with current eslint configuration

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 export default {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "./rules/default.js",
-        "eslint:recommended",
-    ],
-    "parserOptions": {
-        "sourceType": "module"
-    },
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  plugins: ['vue'],
+  extends: [
+    './rules/default.js',
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  }
 };
--- rules/default.js
+++ rules/default.js
@@ -1,20 +1,17 @@
 export default {
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  rules: {
+    semi: [2, 'never'],
+    'no-console': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'space-before-function-paren': 0,
+    'comma-dangle': ['error', 'always-multiline'],
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'es5'
+      }
+    ]
+  }
 };
